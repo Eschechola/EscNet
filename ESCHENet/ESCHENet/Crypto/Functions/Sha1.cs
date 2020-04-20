f1a40552aa000f477157dbd0f6f0f00e30ae1550
Eschechola <lucas.eschechola@outlook.com>
2020-04-20 10:52:36 -0300

    Refatorando para publicação NUGet

---
+++ ESCHENet/ESCHENet/Crypto/Functions/Sha1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using ESCHENet.Crypto.Interfaces;
+using System.Security.Cryptography;
+
+namespace ESCHENet.Crypto.Functions
+{
+    public class Sha1 : ICrypto
+    {
+        private readonly SHA1CryptoServiceProvider cryptoTransformSHA1;
+
+        public Sha1()
+        {
+            cryptoTransformSHA1 = new SHA1CryptoServiceProvider();
+        }
+
+        public string Generate(string hash)
+        {
+            byte[] buffer = Encoding.Default.GetBytes(hash);
+            string report = BitConverter.ToString(cryptoTransformSHA1.ComputeHash(buffer))
+                                .Replace("-", "")
+                                .ToLower();
+
+            return report;
+        }
+
+        private new static string Encrypt(string text) => string.Empty;
+
+        private new static string Decrypt(string text) => string.Empty;
+    }
+}
---
+++ ESCHENet/ESCHENet/Crypto/Interfaces/ICrypto.cs
@@ -0,0 +1,9 @@
+﻿namespace ESCHENet.Crypto.Interfaces
+{
+    public abstract class ICrypto
+    {
+        public static string Encrypt(string text) => string.Empty;
+        
+        public static string Decrypt(string text) => string.Empty;
+    }
+}
